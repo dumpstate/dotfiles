530fcae1ce1479a76c6250a6b2e68baa9cd49a93
Albert Sadowski <asadowski@mailbox.org>
2023-02-25 14:59:27 +0000

    Add checkdeps script

---
+++ executable_checkdeps.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# There is many ways to install dependencies on different
+# operating systems. All I need is to check whether the dependency
+# is or isn't present.
+
+set -euo pipefail
+
+function main {
+  local deps_file="${HOME}/.deps"
+  if [[ ! -f "$deps_file" ]]; then
+    echo ".deps file does not exist"
+    exit 1
+  fi
+
+  local missing_count=0
+
+  while IFS="" read -r executable || [ -n "$executable" ]; do
+    local path=$(which ${executable})
+
+    if [[ ! -f "$path" || ! -x "$path" ]]; then
+      echo "Missing executable: $executable"
+      missing_count=$((missing_count + 1))
+    fi
+  done < "$deps_file"
+
+  if [[ "${missing_count}" -ge 1 ]]; then
+    exit 1
+  fi
+
+  exit 0
+}
+
+main "$@"
