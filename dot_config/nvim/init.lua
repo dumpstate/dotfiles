a1941197fcdf0e78bb519a26887a683ca7233f73
Albert Sadowski <asadowski@mailbox.org>
2024-02-18 08:34:59 +0000

    nvim with lua

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,61 @@
+vim.opt.number = true            -- line numbers
+vim.opt.ignorecase = true        -- case insensitive search
+vim.opt.hlsearch = true          -- highlight search
+vim.opt.incsearch = true         -- incremental search
+vim.opt.autoindent = true        -- new line at the same indent
+vim.opt.showmatch = true         -- show matching
+vim.opt.wildmode = longest,list  -- bash-like tab completions
+vim.opt.mouse = a                -- allow mouse
+vim.opt.ttyfast = true           -- fast scrolling
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+
+vim.api.nvim_set_keymap("n", "<C-J>", "<C-W><C-J>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-H>", "<C-W><C-H>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-K>", "<C-W><C-K>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-L>", "<C-W><C-L>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<CR>", ":noh<CR><CR>", { noremap = true })
+
+-- install folke/lazy.nvim package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+	{
+	    "nvim-tree/nvim-tree.lua",
+	    dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+})
+
+local telescope_builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", telescope_builtin.find_files, {})
+
+require("nvim-tree").setup()
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", {})
+
+vim.cmd[[colorscheme tokyonight]]
